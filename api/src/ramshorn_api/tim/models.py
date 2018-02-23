f35e844895166d8a32d8fb416874bfeee958cdcf
William Hughes <will@willhughes.name>
2018-02-23 14:50:42 +1300

    Models and serializers for flora and fauna

--- api/src/ramshorn_api/tim/models.py
+++ api/src/ramshorn_api/tim/models.py
@@ -11,7 +11,7 @@ class Tank(models.Model):
     depth = models.DecimalField(max_digits=13, decimal_places=1, help_text="Depth, in cm")
     length = models.DecimalField(max_digits=13, decimal_places=1, help_text="Length, in cm")
     volume_override = models.DecimalField(max_digits=13, decimal_places=1, help_text="Overridden volume, in L", null=True)
-    
+
     @property
     def volume(self):
         if self.volume_override is not None:
@@ -22,3 +22,35 @@ class Tank(models.Model):
     @volume.setter
     def volume(self, value):
         self.volume_override = volume
+
+
+class Classification(models.Model):
+
+    class Meta:
+        abstract = True
+
+    genus = models.TextField()
+    species = models.TextField()
+    variant = models.TextField(blank=True)
+
+    def __str__(self):
+        rep = "{} {}".format(self.genus, self.species)
+        if self.variant != "":
+            rep += ' "{}"'.format(self.variant)
+
+        return rep
+
+
+class FloraType(Classification):
+
+    present_in = models.ManyToManyField(Tank, related_name="flora")
+
+
+class FaunaType(Classification):
+    pass
+
+
+class Individual(models.Model):
+
+    species = models.ForeignKey(FaunaType)
+    present_in = models.ForeignKey(Tank, related_name="fauna")
--- api/src/ramshorn_api/tim/serializers.py
+++ api/src/ramshorn_api/tim/serializers.py
@@ -13,4 +13,40 @@ class TankSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Tank
-        fields = ('id', 'name', 'description', 'width', 'depth', 'length')
+        fields = (
+            'id',
+            'name',
+            'description',
+            'width',
+            'depth',
+            'length',
+            'flora',
+            'fauna'
+        )
+
+    flora = FloraTypeSerializer(many=True)
+    fauna = IndividualSerializer(many=True)
+
+
+class ClassificationSerializer(serializers.Serializer):
+
+    genus = serializers.CharField()
+    species = serializers.CharField()
+    variant = serializers.CharField()
+
+
+class FloraTypeSerializer(ClassificationSerializer):
+    pass
+
+
+class FaunaTypeSerializer(ClassificationSerializer):
+    pass
+
+
+class IndividualSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Individual
+        fields = ('id', 'species', )
+
+    species = FaunaTypeSerializer()
